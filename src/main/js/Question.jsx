f2ef70cce7103ebefd62b97865d0a3ba0011fb8b
Toby Dragon <dr.toby.dragon@gmail.com>
2021-02-26 07:57:04 -0500

    Added a first test printout

--- src/main/js/Question.jsx
+++ src/main/js/Question.jsx
@@ -2,13 +2,13 @@ import {Container} from "react-bootstrap";
 
 export const Question = props => {
     //this could be done with a for loop doing an accumulator, if you prefer
-    const optionComponents = props.questionModel.possibleAnswers.map(possAnswerStr => (<option text={possAnswerStr}>{possAnswerStr}</option>));
+    const optionComponents = props.questionModel.possibleAnswers.map(possAnswerStr => (<option key={possAnswerStr}>{possAnswerStr}</option>));
 
     return (
         <Container>
             <div>{props.questionModel.questionText}</div>
-            <select>
-                <option selected disabled="true">---Select Answer---</option>
+            <select defaultValue="---Select Answer---">
+                <option key="---Select Answer---" disabled={true}>---Select Answer---</option>
                 {optionComponents}
             </select>
         </Container>
