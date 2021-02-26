89a6189e738616a58b65c92e30ffb0b5d597a891
Toby Dragon <dr.toby.dragon@gmail.com>
2021-02-26 07:18:13 -0500

    Added comments

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import {QuestionDemo} from "./test/js/QuestionDemo";
 class App extends React.Component {
   
   render(){
-
-    
+    // Keep this class empty except for a single, top-level component
+    // Then you can manually test any component by dropping it in here
     return (
       <div className="App">
         <QuestionDemo/>
--- src/main/js/Question.jsx
+++ src/main/js/Question.jsx
@@ -1,7 +1,7 @@
 import {Container} from "react-bootstrap";
 
 export const Question = props => {
-
+    //this could be done with a for loop doing an accumulator, if you prefer
     const optionComponents = props.questionModel.possibleAnswers.map(possAnswerStr => (<option text={possAnswerStr}>{possAnswerStr}</option>));
 
     return (
