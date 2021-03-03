76d2902433995f2829dad71dcb607636dd47272b
thegoat <ruppatel11599@gmail.com>
2021-03-03 17:54:11 -0500

    added images and logo and test ideas

--- src/main/js/Question.jsx
+++ src/main/js/Question.jsx
@@ -21,9 +21,11 @@ export const Question = props => {
     //this could be done with a for loop doing an accumulator, if you prefer
     const optionComponents = props.questionModel.possibleAnswers.map(possAnswerStr => (<option key={possAnswerStr}>{possAnswerStr}</option>));
 
+    
     return (
         <Container>
             <div>{props.questionModel.questionText}</div>
+            <img src = {props.questionModel.imgUrl} alt="Images Not Loading"></img>
             <select onChange={handleAnswerChange} defaultValue="---Select Answer---">
                 <option key="---Select Answer---" disabled={true}>---Select Answer---</option>
                 {optionComponents}
