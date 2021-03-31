fc21cb51469d43d62ff818d1cc221da9594863ce
Toby Dragon <dr.toby.dragon@gmail.com>
2021-03-31 17:48:35 -0400

    made questions choosable

--- src/main/js/QuestionList.jsx
+++ src/main/js/QuestionList.jsx
@@ -7,7 +7,21 @@ export const QuestionList = props => {
 
     const [curQuestionIndex, setCurQuestionIndex] = useState(0);
 
+    const prevQuestion = () => {
+        setCurQuestionIndex(curQuestionIndex-1);
+    }
+
+    const nextQuestion = () => {
+        setCurQuestionIndex(curQuestionIndex+1);
+    }
+
     return (
-        <Question querstionModel={props.questionListModel[curQuestionIndex]} />
+        <div>
+            <Question questionModel={props.questionListModel[curQuestionIndex]} />
+            <button onClick={prevQuestion}>prev</button>
+            <button onClick={nextQuestion}>next</button>
+        </div>
+        
+
     );
 }
