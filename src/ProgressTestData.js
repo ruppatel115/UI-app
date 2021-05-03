d3dd2361a7c56ec60b9182633063fced22595626
kppalmo <kpalmo@ithaca.edu>
2021-05-03 08:27:31 -0400

    addedData

---
+++ src/ProgressTestData.js
@@ -0,0 +1,12 @@
+export const example = {
+    "plane":"xx.o",
+    "structure":"...o", 
+    "attachment":"oo.o", 
+    "zone":"oo.x"
+};
+export const exampleB = {
+    "plane":"xoxo",
+    "structure":"xxoo", 
+    "attachment":"o..o", 
+    "zone":"ox.."
+};
