a24341392d010e378835290aecbaa594e1dd37df
kppalmo <kpalmo@ithaca.edu>
2021-05-03 08:24:47 -0400

    pdjs

---
+++ src/test/js/ProgressDemo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export const ProgressDemo = props => {
+
+
+    for(const key of Object.keys(progressData)){
+        //call CircularPBar.JSX for each key
+
+    }
+
+
+    
+    return (
+        //Loop using FOR IN => call CircularPBar ONCE for each key:value pair in data
+        // => num of CircularBars = Num of key:value pairs
+        <div>
+        
+           
+        </div>
+    )
+}
