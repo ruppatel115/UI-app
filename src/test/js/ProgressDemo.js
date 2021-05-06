3fdea8e0077755df18c07d3a91bf09f90c6fcdd2
kppalmo <kepalmo8@gmail.com>
2021-05-06 08:20:54 -0400

    addedMoreTests

--- src/test/js/ProgressDemo.js
+++ src/test/js/ProgressDemo.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {example} from '../resources/ProgressTestData';
+import {example,exampleB, exampleC} from '../resources/ProgressTestData';
 import {ProgressCircles} from '../../main/js/ProgressCircles';
-import {exampleB} from '../resources/ProgressTestData';
 
 export const ProgressDemo = props => {
     
     return (
 
         <div>
-            <ProgressCircles progModel={exampleB}/>   
+            {/* <ProgressCircles progModel={exampleC}/>   \ */}
+            <ProgressCircles progModel={example}/>
+            {/* <ProgressCircles progModel={exampleB}/>   */}
         </div>
     )
 }
--- src/test/resources/ProgressTestData.js
+++ src/test/resources/ProgressTestData.js
@@ -4,7 +4,7 @@ export const example = {
     "attachment":"oo.o", 
     "zone":"oo.x",
 };
-//using more then 4 obj
+//using more then 4 keys
 export const exampleB = {
     "math":"xoxo",
     "science":"xxoo", 
@@ -15,4 +15,13 @@ export const exampleB = {
     "gym":"xxxx",
     "elective":"x.o.",
     "test":"o.x."
+};
+//using more then 4 values
+export const exampleC = {
+    "math":"xo..ox.oxo",
+    "science":"xxo.ox.ox.", 
+    "history":"xxx..oxx..", 
+    "english":"xxxxxxxxxx",
+    "spanish":"..........",
+    "drawing":"oooooooooo",
 };
